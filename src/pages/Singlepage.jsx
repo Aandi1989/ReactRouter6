4652a3ee80c488439f68a1f9698b50982c8e14cf
Aandi1989 <navaliny1989@gmail.com>
2023-08-23 11:19:19 +0300

    useNavigate & goBack

--- src/pages/Singlepage.jsx
+++ src/pages/Singlepage.jsx
@@ -1,9 +1,13 @@
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 export const Singlepage = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [post, setPost] = useState(null);
+
+  const goBack = () => navigate(-1);
+  const goHome = () => navigate('/', {replace: true})
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -12,6 +16,9 @@ export const Singlepage = () => {
 }, [id]);
   return (
     <div>
+        <button className='border p-0.5 rounded mt-4 mr-4' onClick={goBack}>Go back</button>
+        <button className='border p-0.5 rounded mt-4' onClick={goHome}>Go home</button>
+        {/* Bad approach. It's better to use Link instead goHome */}
         {post && (
             <>
                 <h1 className="text-2xl font-semibold mb-2 text-center py-5">{post.title}</h1>
